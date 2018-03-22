5ccd54cf5caa63a43364ded6fc84bf70572a273b
MargotDem <margot_demaulmont@hotmail.fr>
2018-03-22 22:26:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import Header from './components/Header'
+import { HashRouter } from 'react-router-dom'
+import { Scroller } from './components/scroller'
+
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+          <Header />
+    )
+  }
+}
+
+export default App
