69604b649cd20887ac793395c2cecc9884b2de96
MargotDem <margot_demaulmont@hotmail.fr>
2018-03-23 14:54:38 +0100

    top section

--- src/containers/Home/Main.js
+++ src/containers/Home/Main.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
-import './home.css'
+import pic from '../../img/pic.jpg'
+
+import './styles/main.css'
 
 export default class Main extends Component {
   render () {
     return (
     	<div className='main-container'>
+    		<div className='main-message'>
+    			<span><span className='main-H'>H</span>ello</span>&nbsp;<span><span className='main-W'>W</span>orld !</span>
+    		</div>
+    		<div className='main-text'>I’m Margot, a French junior developer, business student, autodidact and language enthusiast</div>
+    		<div className='main-photo-container'><img className='main-photo' alt='me' src={pic} /></div>
       	</div>
     )
   }
