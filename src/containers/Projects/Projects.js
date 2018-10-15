0d5e7bb0da01a4e32d768950d41a20d44d5744c3
MargotDem <margot_demaulmont@hotmail.fr>
2018-10-15 18:50:27 +0200

    added adonis-react project

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -7,6 +7,7 @@ import Project from './Project'
 import projectOcc from '../../img/project-occ.png'
 import projectLexicode from '../../img/project-lexicode.png'
 import projectFlashcardz from '../../img/project-flashcardz.png'
+import projectAdonis from '../../img/project-adonis.png'
 import projectBT from '../../img/project-BT.png'
 import react from '../../img/icons/react.png'
 import php from '../../img/icons/php.png'
@@ -20,6 +21,7 @@ import expressjs from '../../img/icons/expressjs.png'
 import sass from '../../img/icons/sass.png'
 import webpack from '../../img/icons/webpack.png'
 import adonis from '../../img/icons/adonis.png'
+import laravel from '../../img/icons/laravel.png'
 
 import './styles/projects.css'
 
@@ -32,34 +34,23 @@ const PERSO_PROJECTS = [
     picUrl: projectOcc,
     githubUrl: 'https://github.com/MargotDem/opencartecomptable',
     icons: {
-      'php': php,
-      'sql': sql,
       'js': js,
+      'react': react,
+      'php': php,
+      'laravel': laravel,
+      'sql': sql,
       'bootstrap': bootstrap
     }
   },
   {
     id: 2,
-    name: 'Lexicode',
-    url: 'https://lexicode.herokuapp.com/',
-    description: 'projects.lexicode',
-    picUrl: projectLexicode,
-    githubUrl: 'https://github.com/MargotDem/lexicode',
-    icons: {
-      'react': react,
-      'nodejs': nodejs,
-      'sql': sql,
-      'expressjs': expressjs
-    }
-  },
-  {
-    id: 3,
     name: 'Flashcardz',
     url: 'https://the-amazing-flashcardz.herokuapp.com',
     description: 'projects.flashcardz',
     picUrl: projectFlashcardz,
     githubUrl: 'https://github.com/MargotDem/flashcardz',
     icons: {
+      'js': js,
       'nodejs': nodejs,
       'react': react,
       'adonis': adonis,
@@ -67,18 +58,50 @@ const PERSO_PROJECTS = [
       'webpack': webpack,
       'sass': sass
     }
+  },
+  {
+    id: 3,
+    name: 'adonis-react-app',
+    url: 'https://github.com/MargotDem/adonis-react-app',
+    description: 'projects.adonis',
+    picUrl: projectAdonis,
+    githubUrl: 'https://github.com/MargotDem/adonis-react-app',
+    icons: {
+      'js': js,
+      'adonis': adonis,
+      'nodejs': nodejs,
+      'react': react,
+      'webpack': webpack,
+      'sass': sass
+    }
+  },
+  {
+    id: 4,
+    name: 'Lexicode',
+    url: 'https://lexicode.herokuapp.com/',
+    description: 'projects.lexicode',
+    picUrl: projectLexicode,
+    githubUrl: 'https://github.com/MargotDem/lexicode',
+    icons: {
+      'js': js,
+      'react': react,
+      'nodejs': nodejs,
+      'sql': sql,
+      'expressjs': expressjs
+    }
   }
 ]
 
 const CONTRIBUTED_PROJECTS = [
   {
-    id: 4,
+    id: 5,
     name: 'Brother Tongue',
     url: 'https://brothertongue.com/#/',
     description: 'projects.bt',
     picUrl: projectBT,
     githubUrl: false,
     icons: {
+      'js': js,
       'react': react,
       'git': git,
       'terminal': terminal,
